80af226e992a12c7f267a5c842b2c9258bcf82af
supriadiroadman <supriadiroadman@gmail.com>
2020-01-23 23:57:34 +0700

    Setting views & Add Role relation to User

--- app/User.php
+++ app/User.php
@@ -36,4 +36,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // Relation User to Role => One To Many (Inverse)
+    public function role()
+    {
+        return $this->belongsTo('App\Role');
+    }
 }
